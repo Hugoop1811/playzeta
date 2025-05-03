34b0af242d22b56d365ab4f7e31dc3130972abb6
migguzp <150238930+migguzp@users.noreply.github.com>
2025-05-03 16:19:09 +0200

    feat:se añade juego Speed Click con su respectiva vista y controlador

---
+++ app/Http/Controllers/SpeedClickController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SpeedClickController extends Controller
+{
+    public function index()
+    {
+        return view('games.speedclick');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WordleController;
+use App\Http\Controllers\SpeedClickController;
 
 Route::get('/wordle', [WordleController::class, 'index'])->name('wordle.index');
 Route::post('/wordle/try', [WordleController::class, 'check'])->name('wordle.check');
+Route::get('/speedclick', [SpeedClickController::class, 'index'])->name('speedclick.index');
+
+
 Route::get('/', function () {
     return view('home');
 });
