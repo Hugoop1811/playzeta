de368b3d9f2be504a0e72381f3782a17361fd713
raudy-mse <raudymolon@gmail.com>
2025-05-25 23:09:27 +0000

    completo modo medium

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -8,11 +8,37 @@
 
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-3xl">
 
+<<<<<<< HEAD
             <a href="{{ route('wordle.index') }}"
                 class="bg-gradient-to-r from-indigo-500 to-purple-600 p-6 rounded-xl shadow-lg hover:scale-105 transition-transform duration-300 flex flex-col items-center text-center">
                 <span class="text-2xl font-bold mb-2">Reto Diario</span>
                 <span class="text-sm text-zinc-200">Adivina la palabra del día</span>
             </a>
+=======
+        <a href="{{ route('wordle.index') }}"
+           class="bg-gradient-to-r from-indigo-500 to-purple-600 p-6 rounded-xl shadow-lg hover:scale-105 transition-transform duration-300 flex flex-col items-center text-center">
+            <span class="text-2xl font-bold mb-2">Reto Diario</span>
+            <span class="text-sm text-zinc-200">Adivina la palabra del día</span>
+        </a>
+
+        <a href="{{ route('speedclick.challenge') }}"
+           class="bg-gradient-to-r from-pink-500 to-rose-600 p-6 rounded-xl shadow-lg hover:scale-105 transition-transform duration-300 flex flex-col items-center text-center">
+            <span class="text-2xl font-bold mb-2">Speed Click: Dianas</span>
+            <span class="text-sm text-zinc-200">Haz clic en todas las dianas</span>
+        </a>
+
+        <a href="{{ route('speedclick.index') }}"
+           class="bg-gradient-to-r from-purple-600 to-indigo-600 p-6 rounded-xl shadow-lg hover:scale-105 transition-transform duration-300 flex flex-col items-center text-center">
+            <span class="text-2xl font-bold mb-2">Speed Click: Velocidad</span>
+            <span class="text-sm text-zinc-200">Pon a prueba tus reflejos</span>
+        </a>
+
+      <a href="{{ route('battleship.index') }}"
+    class="bg-gradient-to-r from-blue-600 to-sky-500 p-6 rounded-xl shadow-lg hover:scale-105 transition-transform duration-300 flex flex-col items-center text-center">
+    <span class="text-2xl font-bold mb-2">Hundir la flota</span>
+    <span class="text-sm text-zinc-200">Conviertete en el cápitan en esta batalla naval</span>
+        </a>
+>>>>>>> 633ec0c (Completado el modo medium)
 
 
             <a href="{{ route('speedclick.challenge') }}"
